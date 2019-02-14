13f3c325b572f784f0adce8fc433e4ce81ddc2e6
Jake Whalen <timothy.j.whalen@saic.com>
2019-02-14 14:56:37 -0500

    Added code to store article content in a sql lite 3 databse.

--- Article Collection/hyperpartisan articles.py
+++ Article Collection/hyperpartisan articles.py
@@ -4,20 +4,17 @@ Created on Wed Feb 13 20:34:49 2019
 
 @author: timothy.whalen
 """
-
+print('packages')
 import xml.etree.ElementTree as ET
 
+#%%
+print('parse articles-training-bypublisher-20181122.xml')
 xmlfile = r'C:\Users\e481340\Documents\GMU MASTERS\DAEN 690\Data\zenodoData\articles-training-bypublisher-20181122\articles-training-bypublisher-20181122.xml'
-
 tree = ET.parse(xmlfile)
 root = tree.getroot()
 
-#Root is the collection of all articles
-root.tag
-root.tail
-root.text
-root.attrib
-
+#%%
+print('extract article content')
 #Create function to return the text between the <article> tag
 def gettext(elem):
     text = elem.text or ""
@@ -32,14 +29,33 @@ articles = root.getchildren()
 #Dictionary for storing returned text from articles
 artDict = {}
 #Loop through and extract article info and text
+i = 1
 for a in articles:
-    print(a)
+    i += 1
     tmp = a.attrib
     tmp['text'] = gettext(a)
     artDict[a.attrib['id']] = tmp
 
+#%%
+print('connect to db')
+#Write data to SQLlite DB
+import sqlite3
+dbfile = r'C:\Users\e481340\Documents\GMU MASTERS\DAEN 690\DAEN690\Article Collection\articles_zenodo.db'
+db = sqlite3.connect(dbfile)
+cursor = db.cursor()
 
+#%%
+print('insert articles to db')
+def post_row(conn, tablename, rec):
+    keys =  '[' + '],['.join(value.keys()) + ']'
+    question_marks = ','.join(list('?'*len(rec)))
+    values = tuple(rec.values())
+    conn.execute('INSERT INTO '+tablename+' ('+keys+') VALUES ('+question_marks+')', values)
 
+for key, value in artDict.items():
+    print(key)
+    post_row(cursor, 'content', value)
 
-
+print('commit writes')
+db.commit()
 
