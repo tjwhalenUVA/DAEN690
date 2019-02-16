63cda773f1dd5623ec4fb42129a22739749d6778
Paul Brinegar <dpbrinegar@Admins-MacBook-Pro.local>
2019-02-16 18:49:38 -0500

    Added matplotlib.pyplot and sent each plot to a separate figure to correct for the plot error Dave was describing in the hangout.

--- EDA/explore_lean.py
+++ EDA/explore_lean.py
@@ -2,6 +2,7 @@
 import sqlite3
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #Setting up paths (redirect path to db)
 _thisFile = os.path.dirname(os.path.abspath('__file__'))
@@ -13,10 +14,13 @@ query = "SELECT * FROM lean;"
 lean = pd.read_sql_query(query, _db)
 
 #Check counts of data leaning
+plt.figure()
 pd.value_counts(lean['hyperpartisan']).plot.bar()
+plt.figure()
 pd.value_counts(lean['bias']).plot.bar()
 
 #Split out and group by just the site
 lean['site'] = lean.url.str.split('//', expand=True)[1].str.split('.', expand=True)[0]
 
+plt.figure()
 pd.value_counts(lean['site'])[0:20].plot.bar()
