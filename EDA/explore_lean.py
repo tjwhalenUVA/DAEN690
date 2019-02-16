6d58d98dba4a42cace2abd31910ffe624c0f2f33
tjwhalenUVA <tjw4ra@virginia.edu>
2019-02-15 23:36:06 -0500

    plot cnts of hyoer and bias

--- EDA/explore_lean.py
+++ EDA/explore_lean.py
@@ -9,9 +9,12 @@ _thisFile = os.path.dirname(os.path.abspath('__file__'))
 _dbfile = r'%s/articles_zenodo.db' % _thisFile.replace("EDA", "Article Collection")
 
 #Connecting to database
+print('---connect to db---')
 _db = sqlite3.connect(_dbfile)
 print('---read in [lean] table---')
 query = "SELECT * FROM lean;"
 lean = pd.read_sql_query(query, _db)
-
-print(lean.head(10))
+#Check counts of data leaning
+print('---plot data---')
+pd.value_counts(lean['hyperpartisan']).plot.bar()
+pd.value_counts(lean['bias']).plot.bar()
