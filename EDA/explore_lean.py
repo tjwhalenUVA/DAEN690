2b75f1b834b9736e7dae7b2e8e6697be409daab7
tjwhalenUVA <tjw4ra@virginia.edu>
2019-02-15 23:15:13 -0500

    Read in lean table

--- EDA/explore_lean.py
+++ EDA/explore_lean.py
@@ -2,10 +2,16 @@
 print('---importing packages---')
 import sqlite3
 import os
+import pandas as pd
 
-#Setting up paths
+#Setting up paths (redirect path to db)
 _thisFile = os.path.dirname(os.path.abspath('__file__'))
 _dbfile = r'%s/articles_zenodo.db' % _thisFile.replace("EDA", "Article Collection")
 
 #Connecting to database
 _db = sqlite3.connect(_dbfile)
+print('---read in [lean] table---')
+query = "SELECT * FROM lean;"
+lean = pd.read_sql_query(query, _db)
+
+print(lean.head(10))
