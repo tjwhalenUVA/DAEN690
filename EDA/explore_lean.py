f550cc3509e44a2bd1c17f9cb6c7984fc836d8e2
tjwhalenUVA <tjw4ra@virginia.edu>
2019-02-15 23:47:36 -0500

    Code added to count up sources and plot top 20

--- EDA/explore_lean.py
+++ EDA/explore_lean.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-print('---importing packages---')
 import sqlite3
 import os
 import pandas as pd
@@ -9,12 +8,15 @@ _thisFile = os.path.dirname(os.path.abspath('__file__'))
 _dbfile = r'%s/articles_zenodo.db' % _thisFile.replace("EDA", "Article Collection")
 
 #Connecting to database
-print('---connect to db---')
 _db = sqlite3.connect(_dbfile)
-print('---read in [lean] table---')
 query = "SELECT * FROM lean;"
 lean = pd.read_sql_query(query, _db)
+
 #Check counts of data leaning
-print('---plot data---')
 pd.value_counts(lean['hyperpartisan']).plot.bar()
 pd.value_counts(lean['bias']).plot.bar()
+
+#Split out and group by just the site
+lean['site'] = lean.url.str.split('//', expand=True)[1].str.split('.', expand=True)[0]
+
+pd.value_counts(lean['site'])[0:20].plot.bar()
