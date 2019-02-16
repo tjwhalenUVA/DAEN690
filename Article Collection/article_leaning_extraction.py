d6f617d85ffeb2ad6395d6be3d21540fe4381666
tjwhalenUVA <tjw4ra@virginia.edu>
2019-02-15 22:07:33 -0500

    Added code to create table 'lean' and commit to db. Fails if table exist

--- Article Collection/article_leaning_extraction.py
+++ Article Collection/article_leaning_extraction.py
@@ -19,3 +19,34 @@ i = 0
 for a in _articles:
     _artDict[i] = a.attrib
     i += 1
+
+print('setting up db')
+#Write data to SQLlite DB
+import sqlite3
+_dbfile = r'%s/articles_zenodo.db' % _thisFile
+
+#Function to insert data into db table (make this a module avaiable between both)
+def post_row(conn, tablename, rec):
+    keys =  '[' + '],['.join(rec.keys()) + ']'
+    question_marks = ','.join(list('?'*len(rec)))
+    values = tuple(rec.values())
+    conn.execute('INSERT INTO '+tablename+' ('+keys+') VALUES ('+question_marks+')', values)
+
+#Check if DB exists
+#if it does just print message; if it doesn't create it and fill it up
+print('creating leaning table')
+_db = sqlite3.connect(_dbfile)
+_cursor = _db.cursor()
+_cursor.execute('''CREATE TABLE lean
+([id] STRING PRIMARY KEY NOT NULL,
+[hyperpartisan] STRING,
+[bias] STRING,
+[url] STRING,
+[labeled-by] STRING);''')
+
+print('insert articles to db')
+for key, value in _artDict.items():
+    print(key)
+    post_row(_cursor, 'lean', value)
+print('commit writes')
+_db.commit()
