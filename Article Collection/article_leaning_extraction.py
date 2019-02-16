3bd3e5690c61f8a1c982fcf91403de151c3d3e9c
tjwhalenUVA <tjw4ra@virginia.edu>
2019-02-15 21:46:40 -0500

    Added loop to grab and store leaning content in py dict

--- Article Collection/article_leaning_extraction.py
+++ Article Collection/article_leaning_extraction.py
@@ -4,3 +4,18 @@ import xml.etree.ElementTree as ET
 import os
 
 _thisFile = os.path.dirname(os.path.abspath('__file__'))
+
+print('parse ground-truth-training-bypublisher-20181122.xml')
+_xmlfile = r'%s/ground-truth-training-bypublisher-20181122.xml' % _thisFile
+_tree = ET.parse(_xmlfile)
+_root = _tree.getroot()
+
+#Create list of articles in xml file
+_articles = _root.getchildren()
+#Dictionary for storing returned metadata for articles
+_artDict = {}
+#Loop through and extract article info and text
+i = 0
+for a in _articles:
+    _artDict[i] = a.attrib
+    i += 1
