7ae1140937c6a4c3b4b1aa3d30ec0ed7279bee5c
tjwhalenUVA <tjw4ra@virginia.edu>
2019-02-17 16:13:05 -0500

    Created a plot for viewing article source counts

--- EDA/visualize_lean.R
+++ EDA/visualize_lean.R
@@ -43,3 +43,30 @@ lean %>%
     theme(legend.position = 'none') +
     labs(x='Article Hyperpartisan', y='# of Articles') +
     scale_y_continuous(labels = scales::comma)
+
+#Most frequent sources
+lean %>%
+    mutate(source = str_split(str_split(url, '//', simplify = T)[,2],
+                              fixed('.'), simplify = T)[,1],
+           bias = factor(bias,
+                         levels = c('left', 'left-center',
+                                    'least',
+                                    'right-center', 'right'))) %>%
+    group_by(source, bias) %>%
+    count() %>%
+    arrange(desc(n)) %>%
+    head(30) %>%
+    ggplot(aes(x= reorder(source, -n), y=n, fill=bias)) +
+    geom_bar(stat = 'identity') +
+    scale_fill_manual(values = c('dodgerblue2', 'lightblue',
+                                 'gray',
+                                 'indianred1', 'firebrick')) +
+    theme_grey() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1),
+          legend.position = c(0.8, 0.6),
+          legend.background = element_rect(color = 'grey30')) +
+    labs(x=NULL, y='# of Articles')
+    
+
+
+
