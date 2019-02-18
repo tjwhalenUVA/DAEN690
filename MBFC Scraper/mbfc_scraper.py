76b9e65116d7964aede5f30621d261d4f50d718e
tjwhalenUVA <tjw4ra@virginia.edu>
2019-02-17 20:19:48 -0500

    Scraped MBFC site and collected all URLs of labelled sites

--- MBFC Scraper/mbfc_scraper.py
+++ MBFC Scraper/mbfc_scraper.py
@@ -5,23 +5,53 @@ import requests
 import os
 
 #Pages to scrape
-pages = {}
-pages['left'] = 'https://mediabiasfactcheck.com/left/'
-pages['leftcenter'] = 'https://mediabiasfactcheck.com/leftcenter/'
-pages['center'] = 'https://mediabiasfactcheck.com/center/'
-pages['rightcenter'] = 'https://mediabiasfactcheck.com/right-center/'
-pages['right'] = 'https://mediabiasfactcheck.com/right/'
+_pages = {}
+_pages['left'] = 'https://mediabiasfactcheck.com/left/'
+_pages['leftcenter'] = 'https://mediabiasfactcheck.com/leftcenter/'
+_pages['center'] = 'https://mediabiasfactcheck.com/center/'
+_pages['rightcenter'] = 'https://mediabiasfactcheck.com/right-center/'
+_pages['right'] = 'https://mediabiasfactcheck.com/right/'
 
-#Get page conents
-page = requests.get(pages['left'])
-# Create a BeautifulSoup object
-soup = BeautifulSoup(page.text, 'html.parser')
+#Lists of sources in bias groups
+_sources = {}
+_sources['left'] = []
+_sources['leftcenter'] = []
+_sources['center'] = []
+_sources['rightcenter'] = []
+_sources['right'] = []
 
-#Get DIV with links in it
-_link_div = soup.find(class_='entry clearfix')
-# Pull text from all instances of <a> tag within BodyText div
-_link_p = _link_div.find_all('p')
-_link_a = _link_p.find_all('a')
+#Loop through biases
+for bias in _pages.keys():
+    print('----------'+bias+'----------')
+    #Get page conents
+    _page = requests.get(_pages[bias])#replace with loop
+    # Create a BeautifulSoup object
+    _soup = BeautifulSoup(_page.text, 'html.parser')
+    #Get DIV with links in it
+    _link_div = _soup.find(class_='entry clearfix')
+    # Get 2nd p section
+    _link_p = _link_div.find_all('p')
+    #Get all links which are the sources
+    _link_a = _link_p[1].find_all('a')
+    for _l in _link_a:
+        print(_l.text)
+        _src_page = requests.get(_l.get('href'))
+        _src_soup = BeautifulSoup(_src_page.text, 'html.parser')
+        for p in _src_soup.find_all('p'):
+            if "Source:" in p.text:
+                try:
+                    _sources[bias].append(p.find_all('a')[0].get('href'))
+                except:
+                    pass
 
-print(_link_a)
-print(len(_link_a))
+import pandas as pd
+_mbfc_df = pd.DataFrame(columns=['bias', 'sourceURL'])
+
+for bias in _sources.keys():
+    _mbfc_df = pd.concat([_mbfc_df,
+                          pd.DataFrame(data = {'bias': [bias] * len(_sources[bias]),
+                                               'sourceURL': _sources[bias]})],
+                         axis=0)
+
+
+
