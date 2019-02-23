910831ecbcb6838e439ca4ba77eec6bf927cc586
David Wilbur <David.C.Wilbur@gmail.com>
2019-02-23 13:55:17 -0500

    updated csv file to remove leading character

--- EDA/simpleDBQueries.py
+++ EDA/simpleDBQueries.py
@@ -14,17 +14,48 @@ import datetime
 
 #Setting up paths (redirect path to db)
 _thisFile = os.path.dirname(os.path.abspath('__file__'))
-_dbfile = r'%s/articles_zenodo.db' % _thisFile.replace("EDA", "Article Collection")
+_dbfile = r'%s/articles_zenodo.db' % _thisFile.replace("EDA","Article Collection")
 
 #Connecting to database
 _db = sqlite3.connect(_dbfile)
-query = "SELECT `published-at` FROM content;"
+query = "SELECT `published-at` \
+         FROM content;"
 lean = pd.read_sql_query(query, _db)
 
 _db = sqlite3.connect(_dbfile)
-query = "SELECT `published-at` FROM content WHERE `published-at` IS NOT NULL AND `published-at` >= '2008-01-01';"
+query = "SELECT `published-at` \
+         FROM content \
+         WHERE `published-at` IS NOT NULL AND \
+               `published-at` >= '2009-01-01';"
+lean = pd.read_sql_query(query, _db)
+
+
+_db = sqlite3.connect(_dbfile)
+query = "SELECT `published-at` \
+         FROM content \
+         WHERE `published-at` IS NULL;"
 lean = pd.read_sql_query(query, _db)
 
 _db = sqlite3.connect(_dbfile)
-query = "SELECT `published-at` FROM content WHERE `published-at` IS NULL;"
+query = "SELECT content.id, lean.id, content.`published-at`, lean.bias \
+         FROM content, lean \
+         WHERE content.id=lean.id AND \
+               content.`published-at` IS NOT NULL AND \
+               content.`published-at` >= '2009-01-01' AND \
+               content.`published-at` <= '2009-12-31' AND \
+               lean.bias = 'right' ;"
 lean = pd.read_sql_query(query, _db)
+
+_db = sqlite3.connect(_dbfile)
+query = "SELECT content.id, lean.id, content.`published-at`, lean.bias \
+         FROM content, lean \
+         WHERE content.id=lean.id AND \
+               content.`published-at` IS NOT NULL AND \
+               content.`published-at` >= '2009-01-01' AND \
+               content.`published-at` <= '2009-12-31' AND \
+               lean.bias = 'right-center' ;"
+lean = pd.read_sql_query(query, _db)
+
+####
+####
+
