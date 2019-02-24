78674c64233aff729bacdcdd1bc1c41a982e5a34
tjwhalenUVA <tjw4ra@virginia.edu>
2019-02-23 22:42:10 -0500

    Script to run for creating database

---
+++ Article Collection/create_db.py
@@ -0,0 +1,8 @@
+print('############## Loading Training Articles ##############')
+import train_article_content_extraction
+print('############## Loading Training Leaning ##############')
+import train_article_leaning_extraction
+print('############## Loading Test Articles ##############')
+import test_article_content_extraction
+print('############## Loading Test Leaning ##############')
+import test_article_leaning_extraction
