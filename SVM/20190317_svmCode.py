940c7cda9ae9679cda432f38cf6cc20cc6485714
David Wilbur <David.C.Wilbur@gmail.com>
2019-03-17 20:35:54 -0400

    Added grid search capability

--- SVM/20190317_svmCode.py
+++ SVM/20190317_svmCode.py
@@ -39,10 +39,7 @@ q2 = """SELECT ln.id, ln.bias_final, cn.text
         FROM test_lean ln, test_content cn
         WHERE cn.`published-at` >= '2009-01-01' AND ln.id == cn.id """
 test_df = pd.read_sql(q2, _db, columns=('id', 'lean', 'text'))
-test_lean = pd.read_sql('select * from train_lean;', _db)
-
-
-
+test_lean = pd.read_sql('select * from test_lean;', _db)
 
 #%% Source to article ID mapping
 
@@ -87,12 +84,52 @@ predicted = text_clf.predict(test_df.text)
 np.mean(predicted == test_df.bias_final)
 
 #%%
-
+import numpy as np
+from time import time
+from operator import itemgetter
+from sklearn.grid_search import GridSearchCV
 from sklearn.linear_model import SGDClassifier
-text_clf_svm = Pipeline([('vect', CountVectorizer()),
+
+# Utility function to report best scores
+def report(grid_scores, n_top=3):
+    top_scores = sorted(grid_scores, key=itemgetter(1), reverse=True)[:n_top]
+    for i, score in enumerate(top_scores):
+        print("Model with rank: {0}".format(i + 1))
+        print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+              score.mean_validation_score,
+              np.std(score.cv_validation_scores)))
+        print("Parameters: {0}\n".format(score.parameters))
+
+
+
+#text_clf_svm = Pipeline([('vect', CountVectorizer()),
+#                         ('tfidf', TfidfTransformer()),
+#                         ('clf-svm', SGDClassifier(loss='hinge', penalty='l2',
+#                                                   alpha=1e-3, n_iter=10, random_state=42)),])
+#text_clf_svm = text_clf_svm.fit(_df.text, _df.bias_final)
+#predicted_svm = text_clf_svm.predict(test_df.text)
+#np.mean(predicted_svm == test_df.bias_final )
+
+
+pipe_svm = Pipeline([('vect', CountVectorizer()),
                          ('tfidf', TfidfTransformer()),
-                         ('clf-svm', SGDClassifier(loss='hinge', penalty='l2',
-                                                   alpha=1e-3, n_iter=5, random_state=42)),])
-text_clf_svm = text_clf_svm.fit(_df.text, _df.bias_final)
-predicted_svm = text_clf_svm.predict(test_df.text)
-np.mean(predicted_svm == test_df.bias_final )
+                         ('clf-svm', SGDClassifier()),])
+
+
+# build a classifier
+#clf = SGDClassifier()
+
+# use a full grid over all parameters
+param_grid = {'clf-svm': [SGDClassifier()],
+                            "clf-svm__n_iter": [1, 5, 10],
+                            "clf-svm__alpha": [0.0001, 0.001, 0.01, 0.1, 1, 10, 100],
+                            "clf-svm__penalty": ["none", "l1", "l2"]}
+ 
+# run grid search
+grid_search = GridSearchCV(pipe_svm, param_grid=param_grid, scoring='accuracy')
+start = time()
+grid_search.fit(_df.text, _df.bias_final)
+ 
+print("GridSearchCV took %.2f seconds for %d candidate parameter settings."
+      % (time() - start, len(grid_search.grid_scores_)))
+report(grid_search.grid_scores_)
